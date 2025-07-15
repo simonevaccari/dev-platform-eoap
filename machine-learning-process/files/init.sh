5308701314093088078e6ea1ad28da17e6438e01
Simone Vaccari <simone.vaccari@terradue.com>
2025-07-15 15:45:01 +0200

    edits to install hatch

--- machine-learning-process/files/init.sh
+++ machine-learning-process/files/init.sh
@@ -23,23 +23,23 @@ echo '{"workbench.colorTheme": "Visual Studio Dark"}' > /workspace/User/settings
 # Setup Python virtual environment
 python -m venv /workspace/.venv
 source /workspace/.venv/bin/activate
-/workspace/.venv/bin/python -m pip install --no-cache-dir tomlq calrissian yq backports.zoneinfo
+/workspace/.venv/bin/python -m pip install --no-cache-dir hatch tomlq calrissian yq
 
 
-echo "**** install kubectl ****" 
-curl -s -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"  
-chmod +x kubectl 
-mkdir -p /workspace/.venv/bin                                                                                                   
-mv ./kubectl /workspace/.venv/bin/kubectl
+# echo "**** install kubectl ****" 
+# curl -s -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"  
+# chmod +x kubectl 
+# mkdir -p /workspace/.venv/bin                                                                                                   
+# mv ./kubectl /workspace/.venv/bin/kubectl
 
 
-curl -s -L https://github.com/pypa/hatch/releases/latest/download/hatch-x86_64-unknown-linux-gnu.tar.gz | tar -xzvf - -C /workspace/.venv/bin/
-chmod +x /workspace/.venv/bin/hatch
-curl -s -L https://github.com/go-task/task/releases/download/v3.41.0/task_linux_amd64.tar.gz | tar -xzvf - -C /workspace/.venv/bin/
-chmod +x /workspace/.venv/bin/task
+# curl -s -L https://github.com/pypa/hatch/releases/latest/download/hatch-x86_64-unknown-linux-gnu.tar.gz | tar -xzvf - -C /workspace/.venv/bin/
+# chmod +x /workspace/.venv/bin/hatch
+# curl -s -L https://github.com/go-task/task/releases/download/v3.41.0/task_linux_amd64.tar.gz | tar -xzvf - -C /workspace/.venv/bin/
+# chmod +x /workspace/.venv/bin/task
 
-curl -s -L https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 > /workspace/.venv/bin/skaffold
-chmod +x /workspace/.venv/bin/skaffold
+# curl -s -L https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 > /workspace/.venv/bin/skaffold
+# chmod +x /workspace/.venv/bin/skaffold
 
 
 
